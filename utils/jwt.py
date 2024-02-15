b254199d2c91453e58422dd0e1c900cc0c823508
feiandxs <yiziyint@gmail.com>
2024-02-15 22:11:36 +0800

    jwt token util update

--- utils/jwt.py
+++ utils/jwt.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author: feiandxs
+from builtins import str, bytes
+
+from pydantic import BaseModel
+from typing import Any, Optional
 from authlib.jose import jwt
+from datetime import datetime, timedelta
+
+
+class JWTMeta(BaseModel):
+    exp: int  # 过期时间（Expiration Time）
+    sub: Optional[Any]  # 主题（Subject），通常是用户ID
+    iat: int  # 签发时间（Issued At）
+    iss: str  # 签发者（Issuer）
+
 
 # 定义JWT的头部，指定算法（alg）和类型（typ）
 header = {
@@ -9,27 +22,28 @@ header = {
     "typ": "JWT"
 }
 
-# 定义JWT的载荷，包含一些声明（claims）
-payload = {
-    "sub": "1234567890",  # 主题（Subject），通常是用户ID
-    "name": "John Doe",
-    "iat": 1516239022,  # 签发时间（Issued At）
-    "exp": 1516239022 + 60,  # 过期时间（Expiration Time）
-}
 
-# 生成密钥（Secret），用于签名
-secret = "your-secret-key"
+def generate_token(payload: JWTMeta, secret: str) -> bytes:
+    # current_timestamp = datetime.now().timestamp()
+    # payload demo
+    # payload = {
+    #     "sub": payload.sub,  # 主题（Subject），通常是用户ID
+    #     "user_id": payload.user_id,
+    #     "iat": current_timestamp,  # 签发时间（Issued At）
+    #     "exp": current_timestamp + 60,  # 过期时间（Expiration Time）
+    # }
 
-# 使用jwt.encode()方法生成JWT
-token = jwt.encode(header, payload, secret)
+    # 生成密钥（Secret），用于签名
+    # secret = "your-secret-key"
+    token = jwt.encode(header, payload, secret)
+    return token
 
-# todo
 
-# 使用jwt.decode()方法验证JWT
-try:
-    claims = jwt.decode(token, secret, algorithms=["HS256"])
-    print(claims)  # 如果JWT有效，将打印出解码后的载荷
-except jwt.ExpiredSignatureError:
-    print("Token has expired.")
-except jwt.InvalidTokenError:
-    print("Invalid token.")
+def validate_token(token: str, secret: str) -> Any:
+    try:
+        claims = jwt.decode(token, secret, algorithms=["HS256"])
+        return claims
+    except jwt.ExpiredSignatureError:
+        return "Token has expired."
+    except jwt.InvalidTokenError:
+        return "Invalid token."
